35fbd14aeb45eb8f2f8657f73c03c25583d2b99b
Hussein Jaber <hujaber@me.com>
2021-01-19 23:34:05 +0200

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Hussein Jaber on 19/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
